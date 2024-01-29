68ea2fa70853bc14b1379d1f2bf5fe72d5540a60
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-01-29 13:22:55 -0800

    Add fc: prefix and fly launch config

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const TAG_PREFIXES = ["og:", "fc:"] as const;
+export const TAG_PREFIXES = ["og:", "fc:", "xmtp:"] as const;
 export const PORT = 8080;
 export const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
